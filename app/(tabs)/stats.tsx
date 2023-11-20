d58510de8e0e5bb7f13f10b683958f6840bf275e
dragonslayrr <60010156+dragonslayrr@users.noreply.github.com>
2023-11-20 13:28:55 -0500

    Update stats.tsx

--- app/(tabs)/stats.tsx
+++ app/(tabs)/stats.tsx
@@ -6,7 +6,7 @@ import { Text, View } from '../../components/Themed';
 export default function Stats() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Stats</Text>
+      <Text style={styles.title}>Here are your stats</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
