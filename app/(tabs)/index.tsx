3847a34bd22e562def661de9a99181d33b9a61ca
dragonslayrr <60010156+dragonslayrr@users.noreply.github.com>
2023-11-20 17:17:26 -0500

    1234

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Sol</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text lightColor="blue" darkColor="green" style={styles.subtitle}>Bob Ross</Text>
+      <Text lightColor="blue" darkColor="green" style={styles.subtitle}>Bob Ross aka Kyle</Text>
     </View>
   );
 }
