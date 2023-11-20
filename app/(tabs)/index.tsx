675688edab4f8f3c9a7a0cda3e770416365aca38
dragonslayrr <60010156+dragonslayrr@users.noreply.github.com>
2023-11-20 16:43:29 -0500

    dify

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Sol</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.subtitle}>Official Server Test</Text>
+      <Text style={styles.subtitle}>Did it fix yet</Text>
     </View>
   );
 }
