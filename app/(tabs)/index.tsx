2f66630c38afe0436d0104c335ebba1d68216726
dragonslayrr <60010156+dragonslayrr@users.noreply.github.com>
2023-11-20 22:44:03 -0500

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Sol</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text>Test</Text>
     </View>
   );
 }
