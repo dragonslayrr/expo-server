da9b0ca88ee985c9cacb34ddc3505c043e89b3f3
dragonslayrr <60010156+dragonslayrr@users.noreply.github.com>
2023-11-21 11:48:46 -0500

    darn

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,6 @@ export default function Home() {
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Sol</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text>Is it just that page</Text>
     </View>
   );
 }
