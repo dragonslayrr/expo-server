a0547f504fc00d9b483c312db1a5112f02ee4b94
dragonslayrr <60010156+dragonslayrr@users.noreply.github.com>
2023-11-21 14:04:16 -0500

    defined dam

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -59,19 +59,4 @@ export default function TabLayout() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 25,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+const styles;
