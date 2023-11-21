9914ad337d6e5670a1d72e25feec5e6b7a35b37c
dragonslayrr <60010156+dragonslayrr@users.noreply.github.com>
2023-11-21 11:48:23 -0500

    test push like 30 something

--- app/(tabs)/stats.tsx
+++ app/(tabs)/stats.tsx
@@ -8,7 +8,7 @@ export default function Stats() {
     <View style={styles.container}>
       <Text style={styles.title}>Here are your stats</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text>Hi Donovan</Text>
+      <Text>Hi Aiden</Text>
     </View>
   );
 }
