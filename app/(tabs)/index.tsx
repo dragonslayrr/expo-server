b16df3314f246072175931c9a3a226ed591f3ab7
dragonslayrr <60010156+dragonslayrr@users.noreply.github.com>
2023-11-21 12:19:04 -0500

    color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { Text, View } from '../../components/Themed';
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Sol</Text>
+      <Text style={styles.title} darkColor="white" lightColor="black">Welcome to Sol</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   );
