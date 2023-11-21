e74d7f3e243dded63fc5b8391cca6faa6d2dc5a7
dragonslayrr <60010156+dragonslayrr@users.noreply.github.com>
2023-11-21 07:18:41 -0500

    Update stats.tsx

--- app/(tabs)/stats.tsx
+++ app/(tabs)/stats.tsx
@@ -8,7 +8,7 @@ export default function Stats() {
     <View style={styles.container}>
       <Text style={styles.title}>Here are your stats</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Text>Hi Donovan</Text>
     </View>
   );
 }
