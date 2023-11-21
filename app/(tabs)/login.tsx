6a09515deddb3c5b198335cb249f1c52698dec07
dragonslayrr <60010156+dragonslayrr@users.noreply.github.com>
2023-11-21 13:42:14 -0500

    depracated

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,13 +1,12 @@
-import { StyleSheet, TextInput, Button, TouchableOpacity } from "react-native";
-import { useRef } from "react";
+import { StyleSheet, TextInput, Button, Pressable } from "react-native";
+import { useRef, useEffect, useState } from "react";
 // import bcrypt from "bcrypt";
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 
 export default function Login() {
   const PasswordRef = useRef();
-
-  let email: string;
+  const EmailRef = useRef();
 
   async function hashPassword(password: string): { hashedPassword: string; salt: string } {
     const salt = await bcrypt.genSalt(10);
@@ -18,7 +17,6 @@ export default function Login() {
   async function parseHash(hashedPassword: string, salt: string, password: string): boolean {
     return hashedPassword == (await bcrypt.hash(password, salt));
   }
-
   function handleLogin() {
     //   hashPassword();
     console.log(email);
@@ -27,25 +25,28 @@ export default function Login() {
   function handleSignup() {
     console.log(PasswordRef.current.value);
   }
-
+  function connectToSQLServer() {}
+  useEffect(() => {
+    connectToSQLServer();
+  }, []);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login / Signup</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <View style={styles.loginForm}>
         <Text style={styles.loginText}>Email: </Text>
-        <TextInput id="emailBox" onChangeText={(text) => (email = text)} style={styles.loginFormContent} placeholder="Email" />
+        <TextInput id="emailBox" ref={EmailRef} style={styles.loginFormContent} placeholder="Email" />
         <Text id="passwordBox" style={styles.loginText}>
           Password:{" "}
         </Text>
         <TextInput ref={PasswordRef} style={styles.loginFormContent} placeholder="Password" secureTextEntry />
         <View style={styles.loginButtonView}>
-          <TouchableOpacity onPress={handleLogin} style={styles.loginButtons}>
+          <Pressable onPress={handleLogin} style={styles.loginButtons}>
             <Text>  Login  </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={handleSignup} style={styles.loginButtons}>
+          </Pressable>
+          <Pressable onPress={handleSignup} style={styles.loginButtons}>
             <Text>  Signup  </Text>
-          </TouchableOpacity>
+          </Pressable>
         </View>
       </View>
     </View>
@@ -111,3 +112,5 @@ const styles = StyleSheet.create({
 // VIEW = DIV SO SPAM VIEW (VIEW FUNNY)
 
 // HASH PASSWORD DO NOT JUST SAVE IT DUMMY
+//   npm i mssql
+// https://www.npmjs.com/package/mssql
