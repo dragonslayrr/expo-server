f16668eabfdb315f8625822d5fc5b6083647ef41
dragonslayrr <60010156+dragonslayrr@users.noreply.github.com>
2023-11-21 13:58:36 -0500

    aah

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={main.keyboard}>
+    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} >
       <Tabs
         screenOptions={{
           tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
