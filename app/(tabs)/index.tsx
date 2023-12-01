fdbccde1c93532a60a3d3748043dcbb3d72bc3ff
dragonslayrr <connerszak@gmail.com>
2023-12-01 12:53:04 -0500

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
     <View style={styles.container}>
       <Text style={styles.title} darkColor="white" lightColor="black">Welcome to Sol</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text>Am I rate limited</Text>
     </View>
   );
 }
